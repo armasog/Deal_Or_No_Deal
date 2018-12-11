3cea5bff5a44c33cd1631b9cbf4dc4c6de94284e
armasog <43734687+armasog@users.noreply.github.com>
2018-12-11 13:23:50 -0500

    Added Gameplay Functionality

--- game.py
+++ game.py
@@ -1,3 +1,22 @@
-import game_engine
-import config
+import game_engine as ge
 
+print('Welcome to Deal Or No Deal!')
+ge.print_board()
+print('Choose your case to keep throughout the game!')
+player_case = ge.pick_case()
+print('Great! Let\'s play Deal or No Deal!')
+ge.print_board()
+
+ge.play_round(6)
+ge.play_round(5)
+ge.play_round(4)
+ge.play_round(3)
+ge.play_round(2)
+ge.play_round(1)
+ge.play_round(1)
+ge.play_round(1)
+ge.play_round(1)
+ge.play_round(1)
+
+#  TODO: Finish the game by opening the players case and evaluating the deal
+#  TODO: Add automated testing
--- game_engine.py
+++ game_engine.py
@@ -1,10 +1,14 @@
 import random
 import config
 import copy
+import time
 
 amounts = copy.deepcopy(config.dollar_values)
 board_case_numbers = copy.deepcopy(config.case_numbers)
-round = 1
+game_round = 1
+past_offers = []
+game_over = False
+
 
 def fill_cases():
     cases = {}
@@ -23,16 +27,13 @@ def print_board():
     print(amounts)
 
 
-def initial_case():
-    return input('Select a case to keep throughout the game: ')
-
-
 def pick_case():
     global board_case_numbers
     selection = input('Select a case: ')
     try:
         selection = int(selection)
         if selection in board_case_numbers:
+            board_case_numbers[board_case_numbers.index(selection)] = 'X'  # Replace opened cases with Xs
             return selection
         else:
             print('Please select a valid case. ')
@@ -44,7 +45,6 @@ def pick_case():
 
 def open_case(case):
     global board_cases, board_case_numbers, amounts
-    board_case_numbers[board_case_numbers.index(case)] = 'X'  # Replace opened cases with Xs
     amounts[amounts.index(board_cases[case])] = 'X'  # Replace found amounts with Xs
     return board_cases[case]
 
@@ -52,7 +52,7 @@ def open_case(case):
 def banker_offer():
     global amounts, round
     value_of_all_cases = 0
-    cases_outstanding = 0
+    cases_outstanding = 1  # Starts at one to account for the player's selected case
     for case in board_case_numbers:
         if case != 'X':
             cases_outstanding += 1
@@ -62,5 +62,39 @@ def banker_offer():
     expected_value = value_of_all_cases / cases_outstanding
     # Sucker discount exists so that offers start well below the EV and approach it as the game goes on
     # There is an element of randomness so that offers are not predictable and are more similar to the show
-    sucker_discount = (0.73 / (float(round) + (random.uniform(-0.15, 0.15))))
-    return expected_value * (1 - sucker_discount)
+    sucker_discount = (0.73 / (float(game_round) + (random.uniform(-0.15, 0.15))))
+    final_offer = expected_value * (1 - sucker_discount)
+    return final_offer
+
+
+def deal_or_no_deal():
+    decision = input('Deal (D)... or No Deal(any key)?')
+    decision = decision.upper()
+    return decision == 'D'
+
+
+def exit_game(winnings):
+    global game_over
+    print('Congratulations! You have finished the game with {}'.format(winnings))
+    game_over = True
+
+
+def play_round(number_to_open):
+    global past_offers
+    if not game_over:
+        i = 1
+        print('Please select {} cases!'.format(number_to_open))
+        while i <= number_to_open:
+            print(open_case(pick_case()))
+            print_board()
+            i += 1
+        print('What a round! Time for our first offer...')
+        time.sleep(5)
+        offer = round(banker_offer())
+        print('Past offers: ')
+        print(past_offers)
+        print('The banker offers: {}'.format(offer))
+        if deal_or_no_deal():
+            exit_game(offer)
+        else:
+            past_offers.append(offer)
