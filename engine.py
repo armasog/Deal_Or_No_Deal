194effe4d5ba0f6edcc354ae339917e60dab06ea
armasog <43734687+armasog@users.noreply.github.com>
2018-12-11 12:15:04 -0500

    Fixed Banker Offer Formula

--- engine.py
+++ engine.py
@@ -1,18 +1,18 @@
-import unittest
 import random
 import config
+import game
 import copy
 
 amounts = copy.deepcopy(config.dollar_values)
 board_case_numbers = copy.deepcopy(config.case_numbers)
-round = 1
+
 
 def fill_cases():
     cases = {}
     shuffled_amounts = copy.deepcopy(config.dollar_values)
     random.shuffle(shuffled_amounts)
     for i in config.case_numbers:
-        cases[i] = shuffled_amounts[i-1]
+        cases[i] = shuffled_amounts[i - 1]
     return cases
 
 
@@ -22,6 +22,11 @@ board_cases = fill_cases()  # Fill each case with a dollar value
 def print_board():
     print(board_case_numbers)
     print(amounts)
+
+
+def initial_case():
+    return input('Select a case to keep throughout the game: ')
+
 
 def pick_case():
     global board_case_numbers
@@ -37,26 +42,26 @@ def pick_case():
         print('Please enter an integer. ')
         return pick_case()
 
+
 def open_case(case):
     global board_cases, board_case_numbers, amounts
     board_case_numbers[board_case_numbers.index(case)] = 'X'  # Replace opened cases with Xs
     amounts[amounts.index(board_cases[case])] = 'X'  # Replace found amounts with Xs
     return board_cases[case]
 
+
 def banker_offer():
-    global round, amounts
-    expected_value = 0
+    global amounts
     value_of_all_cases = 0
     cases_outstanding = 0
-    sucker_discount = 0.0
-    max_value = max([i for i in amounts if isinstance(i, int)])
     for case in board_case_numbers:
         if case != 'X':
             cases_outstanding += 1
     for value in amounts:
         if value != 'X':
             value_of_all_cases += value
-    expected_value = value_of_all_cases/cases_outstanding
-    #FIXME - Banker formula is all off
-    return 12275 + (0.748 * expected_value) + (-0.040 * max_value) + (0.0000006986 * (expected_value*expected_value)) + (32.623 * (cases_outstanding*cases_outstanding))
-
+    expected_value = value_of_all_cases / cases_outstanding
+    # Sucker discount exists so that offers start well below the EV and approach it as the game goes on
+    # There is an element of randomness so that offers are not predictable and are more similar to the show
+    sucker_discount = (0.73 / (float(game.round) + (random.uniform(-0.15, 0.15))))
+    return expected_value * (1 - sucker_discount)
