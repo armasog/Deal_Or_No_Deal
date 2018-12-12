20390b7c49cfd231f859803ef478b951b0ff917e
armasog <43734687+armasog@users.noreply.github.com>
2018-12-11 22:23:39 -0500

    Completed test suite

--- tests.py
+++ tests.py
@@ -7,11 +7,17 @@ class TestSuite(unittest.TestCase):
     def test_pick_case_valid_number(self):
         with mock.patch('builtins.input', return_value=2):
             assert ge.pick_case() == 2
+            ge.board_case_numbers[1] = 2  # Reset the application after test is complete
 
     def test_x_out_opened_case(self):
-        with mock.patch('builtins.input', return_value=1):
-            ge.open_case(ge.pick_case())
-        assert ge.board_case_numbers[0] == 'X'
+        with mock.patch('builtins.input', return_value=2):
+            picked_case = ge.pick_case()
+            amounts_index = ge.amounts.index(ge.board_cases[picked_case])
+            original_amount = ge.amounts[amounts_index]
+            ge.open_case(picked_case)
+            assert ge.board_case_numbers[1] == 'X'
+        ge.amounts[amounts_index] = original_amount  # Reset the application after test is complete
+        print(ge.amounts)
 
     def test_deal_or_no_deal_bool(self):
         with mock.patch('builtins.input', return_value='D'):
